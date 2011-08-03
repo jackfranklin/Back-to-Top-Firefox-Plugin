0e61f4d7e539dec1c724d31abc8c09f309ecbd47
Jack Franklin <jack@jackfranklin.co.uk>
2011-08-03 02:11:19 -0400

    First version now working

---
+++ chrome/content/backtotop.js
@@ -0,0 +1,20 @@
+var backToTop = {};
+
+
+backToTop.run = function() {
+  var head = content.document.getElementsByTagName("head")[0],
+      style = content.document.getElementById("back-to-top-style");
+
+  if(!style) {
+    style = content.document.createElement("link");
+		style.id = "back-to-top-style";
+		style.type = "text/css";
+		style.rel = "stylesheet";
+		style.href = "chrome://backtotop/skin/skin.css";
+		head.appendChild(style);
+  }
+  
+  
+  content.scroll(0,0);
+}
+
